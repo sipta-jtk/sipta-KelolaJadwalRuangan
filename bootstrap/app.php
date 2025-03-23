55cd1dbca825268afe4ce09d25101896c4e18168
tmfadhli12 <thoriqmf123@gmail.com>
2025-03-23 23:55:42 +0700

    Update API prefix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        apiPrefix: 'penjadwalan-ruangan/api',
+        apiPrefix: '/api/penjadwalan-ruangan',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
