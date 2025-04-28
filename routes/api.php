3dc2834f38f28b6df436fd47c94ffccaa7aaa37f
Arnan <arnanda.prasatya.tif422@polban.ac.id>
2025-04-28 14:50:07 +0700

    memperbaiki sebelum merge use api controlller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UnggahPenjadwalanController;
+use App\Http\Controllers\PenjadwalanController;
 use App\Http\Controllers\RuanganController;
 
 Route::get('/user', function (Request $request) {
