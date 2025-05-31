f4d68df65ecbe0e189416a1d6ec25602577d9bba
hanswryy <farhan.hans257@gmail.com>
2025-05-31 19:13:44 +0700

    fix forgor the Session method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
